20fa34ec3beaae49640774f5e22d08bce0392acd
John Hadden <59323260+jhadden04@users.noreply.github.com>
2020-11-22 09:38:19 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import praw
+from openpyxl import load_workbook
+import random
+import time
+
+workbook = load_workbook("hello_world.xlsx")  # opens up the excel file you are using
+# it must be a file that is in the same folder as the script
+sheet = workbook.active
+reddit = praw.Reddit(client_id='0_w-HYtscWovfg',
+                     client_secret='ZBUvGyUFWio8INkuHPL6Mrx06EIwOQ',  # logs in your reddit account
+                     user_agent='a reddit instance',
+                     username='TheBigBoyJohn123',
+                     password='pogchamp')
+n = 1
+usernames = []
+while True:
+    x = sheet[f'A{n}']
+    if x.value is None:
+        break
+    usernames.append(x.value)
+    n += 1  # this just gets the usernames from the excel and puts them into a list
+
+usedusernames = []  # used usernames go here
+
+
+def message():
+    title = "this is the title"  # title text, you should change this
+    text = ["this is the first text option", "second", "third", "etc"]  # permutations of the text,
+    # you can change this infinitely
+    q = 0
+
+    for i in range(len(usernames)):
+        w = random.randint(1, (len(text) - 1))
+        reddit.redditor(usernames[q]).message(title, text[w])  # messages all the people from the excel file
+
+        if usernames[q] in usedusernames:  # means they can't be messaged twice
+            continue
+        print(f'you just messaged u/{usernames[q]}')  # notifies you if a message has been sent
+        usedusernames.append(usernames[q])
+        time.sleep(40)  # time interval between messages, measured in seconds, you can change this
+        q += 1
+
+
+message()  # calls the message function
